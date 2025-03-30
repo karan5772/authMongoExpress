cfa306bd98e13163d273eaf57d0484ec9fd9d827
karan5772 <karankumar8239@gmail.com>
2025-03-30 20:49:58 +0530

    password hashing and comparision for login updated

--- models/User.models.js
+++ models/User.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const newSchema = new mongoose.Schema({
     name: {
@@ -35,6 +36,19 @@ const newSchema = new mongoose.Schema({
 },
 {timestamps: true,}
 )
+
+
+newSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+  });
+
+newSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
 
 const User = mongoose.model("User", newSchema);
 
